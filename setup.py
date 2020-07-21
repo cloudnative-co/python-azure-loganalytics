90aa507569fe5d68b5c0ff77ad88a216cc416de1
Sebastian <seba@cloudnative.co.jp>
2020-07-21 06:49:13 +0000

    new

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LogAnalytics",
+    version="0.0.1",
+    description="Microsoft Azure Log Analytics SDK for Python 3.6",
+    author="sebastian",
+    author_email="seba@cloudnative.co.jp",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
